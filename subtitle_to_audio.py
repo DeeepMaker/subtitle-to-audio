2475e19b560bf7a30c984568edb8ea18328545c6
holojupe <mesutacar@gmail.com>
2023-02-16 16:40:53 +0300

    Implemented command line args logic

--- subtitle_to_audio.py
+++ subtitle_to_audio.py
@@ -1,3 +1,24 @@
+import sys
+import pyttsx3
+from gtts import gTTS
+
+def generate_audio(file_name, lib):
+    print("generating audio file for {} with {}".format(file_name, lib))    
+
+if __name__ == "__main__":      
+  n_args = len(sys.argv) - 1
+  if n_args == 1:
+      file_name = sys.argv[1]
+      lib = "pyttsx3"
+  elif n_args == 2:
+      file_name = sys.argv[1]
+      lib = sys.argv[2]
+  else:
+      print("Proper usage text place holder")   
+      sys.exit()
+
+  generate_audio(file_name=file_name, lib=lib)    
+
 # import pyttsx3
 # engine = pyttsx3.init()
 
@@ -14,6 +35,8 @@
 # engine.save_to_file("This is the second text", "aud2.mp3")
 # engine.runAndWait()
 
+"""
 from gtts import gTTS
 tts = gTTS('This is the first text', lang='en')
 tts.save('hello.mp3')
+"""
