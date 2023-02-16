9589be69c0480dd17232d0e4339cde7fe94e67e3
holojupe <mesutacar@gmail.com>
2023-02-16 22:13:40 +0300

    Using argparse for command line arguments.

--- subtitle_to_audio.py
+++ subtitle_to_audio.py
@@ -1,23 +1,42 @@
-import sys
+import os
+import argparse
+from pysubparser import parser
 import pyttsx3
 from gtts import gTTS
 
-def generate_audio(file_name, lib):
-    print("generating audio file for {} with {}".format(file_name, lib))    
+def generate_audio(path, tts_tool="pyttsx3", rate=125):
+  print("Generating audio file for {} with {}".format(path, tts_tool))      
+
+  subtitles = parser.parse(path)
+
+  if tts_tool == "pyttsx3":
+    engine = pyttsx3.init()
+    engine.setProperty('rate', rate)
+    engine.setProperty('voice', engine.getProperty('voices')[1].id)
+
+    #print(engine.getProperty('voices')[1])
+
+    for subtitle in subtitles:
+       print(subtitle.text)
+       engine.
+       engine.say(subtitle.text)
+       engine.runAndWait()          
+  elif tts_tool == "gtts":
+    engine = gTTS('This is the first text', lang='en')
+    engine.save(os.path.splitext(path)[0] + '.mp3')
+  else:
+    print("Unknown converter")
+ 
 
 if __name__ == "__main__":      
-  n_args = len(sys.argv) - 1
-  if n_args == 1:
-      file_name = sys.argv[1]
-      lib = "pyttsx3"
-  elif n_args == 2:
-      file_name = sys.argv[1]
-      lib = sys.argv[2]
-  else:
-      print("Proper usage text place holder")   
-      sys.exit()
+  arg_parser = argparse.ArgumentParser()
+  arg_parser.add_argument("-p", "--path", help="subtitle file path", required=True)
+  arg_parser.add_argument("-t", "--ttstool", help="TTs tool", choices=["pyttsx3", "gtts"], default="pyttsx3")
 
-  generate_audio(file_name=file_name, lib=lib)    
+  args = arg_parser.parse_args()
+
+  print(args)
+  generate_audio(path=args.path, tts_tool=args.ttstool)    
 
 # import pyttsx3
 # engine = pyttsx3.init()
