eb25536940f64a2d6be4a22a0d8ccbdc4d92eb1e
holojupe <mesutacar@gmail.com>
2023-02-18 00:04:51 +0300

    Added silence logic

--- subtitle_to_audio.py
+++ subtitle_to_audio.py
@@ -5,48 +5,58 @@ from pysubparser import parser
 from pydub import AudioSegment
 import pyttsx3
 from gtts import gTTS
+from datetime import timedelta
 
-def generate_audio(path, tts_tool="pyttsx3", rate=125):  
+def time_to_ms(time):
+  return ((time.hour * 60 + time.minute) * 60 + time.second) * 1000 + time.microsecond / 1000
+
+def generate_audio(path, tts_tool="pyttsx3", rate=200):  
   print("Generating audio file for {} with {}".format(path, tts_tool))      
 
   subtitles = parser.parse(path)
 
   if tts_tool == "pyttsx3":    
-    engine = pyttsx3.init()
-    engine.setProperty('rate', rate)
-    engine.setProperty('voice', engine.getProperty('voices')[0].id)
+    tts_engine = pyttsx3.init()
+    tts_engine.setProperty('rate', rate)
+    tts_engine.setProperty('voice', tts_engine.getProperty('voices')[0].id)
 
-    segment = AudioSegment.empty()   
-    
-    with tempfile.TemporaryDirectory() as tmpdirname:          
-      print('created temporary directory', tmpdirname)      
+  audio_sum = AudioSegment.empty()   
+  
+  with tempfile.TemporaryDirectory() as tmpdirname:          
+    print('created temporary directory', tmpdirname)            
 
-      temp_file_path = os.path.join(tmpdirname, "temp.wav")
-      for subtitle in subtitles:         
-        print(temp_file_path)                      
-        engine.save_to_file(subtitle.text, temp_file_path)
-        #engine.say(subtitle.text)       
-        #engine.save_to_file(subtitle.text, os.path.splitext(path)[0] + '.mp3')
-        engine.runAndWait()
+    temp_file_path = os.path.join(tmpdirname, "temp.wav")
+    prev_subtitle = None
+    prev_audio_duration_ms = 0
+    for subtitle in subtitles:
+      if tts_tool == "pyttsx3":              
+        tts_engine.save_to_file(subtitle.text, temp_file_path)
+        tts_engine.runAndWait()
+      else:
+        val = gTTS(subtitle.text, lang='en')
+        val.save(temp_file_path)
 
-        #AudioSegment.from_mp3("./test/test.wav") 
-        segment = segment + AudioSegment.from_wav(temp_file_path) 
+      audio_segment = AudioSegment.from_wav(temp_file_path)         
 
-        print(segment.duration_seconds)
-
-      with open(os.path.splitext(path)[0] + '.wav', 'wb') as out_f:
-        segment.export(out_f, format='wav')
-
-        
+      print(subtitle.start, subtitle.text)
       
+      if prev_subtitle is None:
+        silence_duration_ms = time_to_ms(subtitle.start)
+      else:
+        silence_duration_ms = time_to_ms(subtitle.start) - time_to_ms(prev_subtitle.start) - prev_audio_duration_ms
 
+      audio_sum = audio_sum + AudioSegment.silent(duration=silence_duration_ms) + audio_segment                   
+      
+      prev_subtitle = subtitle
+      prev_audio_duration_ms = len(audio_segment)
+
+    with open(os.path.splitext(path)[0] + '.wav', 'wb') as out_f:
+      audio_sum.export(out_f, format='wav')      
+"""
   elif tts_tool == "gtts":
     engine = gTTS('This is the first text', lang='en')
-    engine.save(os.path.splitext(path)[0] + '.mp3')
-  else:
-    print("Unknown converter")
- 
-
+    engine.save(os.path.splitext(path)[0] + '.mp3')  
+"""
 if __name__ == "__main__":      
   arg_parser = argparse.ArgumentParser()
   arg_parser.add_argument("-p", "--path", help="subtitle file path", required=True)
@@ -57,24 +67,3 @@ if __name__ == "__main__":
   print(args)
   generate_audio(path=args.path, tts_tool=args.ttstool)    
 
-# import pyttsx3
-# engine = pyttsx3.init()
-
-# voices = engine.getProperty('voices')       #getting details of current voice
-# #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-# engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
-
-# print(engine.getProperty("rate"))
-# engine.setProperty('rate', 125)
-
-
-# engine.say("I will speak this text")
-# engine.save_to_file("This is the first text", "aud1.mp3")
-# engine.save_to_file("This is the second text", "aud2.mp3")
-# engine.runAndWait()
-
-"""
-from gtts import gTTS
-tts = gTTS('This is the first text', lang='en')
-tts.save('hello.mp3')
-"""
